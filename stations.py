83d236863b6cea26fb8fb576fdb1ceb500273d65
Roar Emaus <roar@xal.no>
2018-04-11 09:41:11 +0200

    ++

--- stations.py
+++ stations.py
@@ -11,7 +11,7 @@ import math
 class Stations(Frost):
     """
     Description:
-        Finds all stations within a square of chosen size from a point with 
+        Finds all stations within a square of chosen size from a point with
         WGS84 coordinates and collects available observational data from each
         station
     """
@@ -48,12 +48,13 @@ class Stations(Frost):
         station_ids = ','.join(i for i in self.stations)
         for station_id in self.stations:
             self.stations[station_id]['available'] = []
+
         sc, r_json = self.get_observations_available_time_series(sources=station_ids)
         for point in r_json['data']:
             self.stations[point['sourceId'][:7]]['available'].append(point)
 
         if data_type:
-            for station_id, data in self.stations.items(): 
+            for station_id, data in self.stations.items():
                 print(f'Available data for station {station_id}:')
                 print('{:25}{:63}{:20}'.format('Valid from','data type', 'time resolution'))
                 for time_series in data['available']:
@@ -71,15 +72,14 @@ class Stations(Frost):
                             f'{time_series["timeResolution"]:20}')
                 print('')
 
-
-
     def nearest_stations(self) -> None:
         """
         Description:
-            Finds all stations in the vicinity of the point given within a square
+            Finds all stations in the vicinity of the point given within a
+            square
         Args:
-            latitude:           latitude coordinate of wanted point in WGS84 
-            longitude:          longitude coordinate of wanted point in WGS84 
+            latitude:           latitude coordinate of wanted point in WGS84
+            longitude:          longitude coordinate of wanted point in WGS84
             length_of_square:   length of side of square to search for stations
         """
         self.station_ids = {}
@@ -87,19 +87,18 @@ class Stations(Frost):
         status_code, response_json = self.get_sources(geometry = f'POLYGON(({polygon}))')
         for data in response_json['data']:
             self.stations[data['id']] = data
-        self.distance() 
+        self.distance()
 
     def calculate_polygon(self) -> str:
-       """
+        """
         Description:
             Creates the latitude/longitude coordinates of corners of a square
             around the point given
         Args:
-            latitude:           latitude coordinate of wanted point in WGS84 
-            longitude:          longitude coordinate of wanted point in WGS84 
+            latitude:           latitude coordinate of wanted point in WGS84
+            longitude:          longitude coordinate of wanted point in WGS84
             length_of_square:   length of side of square to search for stations
         """
-
         one_deg_lat = 110.574 # km
         one_deg_lon = 111.320*math.cos(math.radians(self.latitude)) # km
 
@@ -107,16 +106,16 @@ class Stations(Frost):
 
         length_to_corner_lat = length_half_side/one_deg_lat
         length_to_corner_lon = length_half_side/one_deg_lon
-       
+
         north = self.latitude + length_to_corner_lat
         south = self.latitude - length_to_corner_lat
         east = self.longitude + length_to_corner_lon
         west = self.longitude - length_to_corner_lon
 
         return f'{self.longitude} {north}, {self.longitude} {south},' +\
-               f'{east} {self.latitude}, {west} {self.latitude},'+\
+               f' {east} {self.latitude}, {west} {self.latitude},'+\
                f' {self.longitude} {north}'
-    
+
     def distance(self) -> None:
         """
         Description:
@@ -132,7 +131,6 @@ class Stations(Frost):
             x = (lon_rad - point_lon_rad)*math.cos((point_lat_rad + lat_rad)/2)
             y = lat_rad - point_lat_rad
             self.stations[station]['distance'] = math.sqrt(x*x + y*y)
-        
 
     def oslo_test(self) -> None:
         """
