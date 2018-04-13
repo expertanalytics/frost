4af7c5435ea0f363bc50ca3be9e08f520fe7bbef
Roar Emaus <roar@xal.no>
2018-04-13 13:58:18 +0200

    reverting back to old structure

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='frost',
       version='0.1',
-      py_modules=['frost'],
+      py_modules=['API', 'stations'],
       install_requires=['requests'],
       )
